5608465c7f4bcf3c36803c048ca15ce209e2d299
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-04-05 00:18:07 -0700

    Treat main as a subroutine. Broken.

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+"""
+A simple Forth compiler for Notch's CPU.
+
+J is used as a scratchpad. When necessary, so is I.
+
+SP is used for the main stack. X is used for the call stack.
+
+At the end of the program, the stack is popped into I and J for analysis.
+"""
+
 from struct import pack
 
 (SET, ADD, SUB, MUL, DIV, MOD, SHL, SHR, AND, BOR, XOR, IFE, IFN, IFG, IFB
@@ -16,6 +26,7 @@ drdict = dict((k, v)
     for k, v in zip([POP, PEEK, PUSH, SP, PC, O], range(24, 30)))
 drdict.update(rdict)
 direct_registers = registers + list(drdict.keys())
+
 
 def value(v):
     """
@@ -48,6 +59,7 @@ def value(v):
 
     raise Exception("Couldn't deal with value %r" % (v,))
 
+
 def assemble(op, a, b=None):
     """
     Assemble an opcode and return a str of one to three words.
@@ -66,29 +78,108 @@ def assemble(op, a, b=None):
 
     raise Exception("Couldn't deal with op %r" % (op,))
 
+
 def trampoline(address):
     """
-    Jump to an address. Always two words.
+    Jump to an absolute address. Always two words.
     """
 
     return pack(">HH", 0x7dc1, address)
 
+
+def switch_to_call():
+    """
+    Switch to the call stack.
+
+    You'd better be calling if you do this!
+    """
+
+    ucode = assemble(SET, J, SP)
+    ucode += assemble(SET, SP, X)
+    return ucode
+
+
+def switch_to_main():
+    """
+    Switch to the main stack.
+    """
+
+    ucode = assemble(SET, X, SP)
+    ucode += assemble(SET, SP, J)
+    return ucode
+
+
+def call(target, ret):
+    """
+    Call a subroutine.
+
+    Safety not guaranteed; you might not ever come back.
+    """
+
+    ucode = switch_to_call()
+    ucode += assemble(SET, PUSH, ret)
+    ucode += assemble(SET, PC, target)
+    return ucode
+
+
+def ret():
+    """
+    Return to the caller.
+
+    It's totally possible to return to lala-land if overused.
+    """
+
+    ucode = switch_to_call()
+    ucode += assemble(SET, PC, POP)
+    return ucode
+
+
+def bootloader(start):
+    """
+    Set up stacks and registers, and then jump to a starting point. After
+    things are finished, pop some of the stack to registers, and halt with an
+    illegal opcode.
+    """
+
+    # First things first. Set up the call stack. Currently hardcoded.
+    ucode = assemble(SET, X, 0xd000)
+    # Hardcode the location of the tail, and call.
+    ucode += call(start, 0x6)
+    # And we're off! As soon as we come back down...
+    ucode += switch_to_main()
+    ucode += tail()
+    # Finish off with an illegal opcode.
+    ucode += pack(">H", 0x0)
+    return ucode
+
+
 def subroutine(name, words, pc, context):
     """
     Compile a list of words into a new word and add it to the context.
+
+    All subroutines, including main, are called into; thus they assume that
+    the current stack is the call stack and switch out of it before doing any
+    work. They switch back to the call stack when returning.
     """
 
-    ucode = ""
+    ucode = switch_to_main()
 
     for word in words:
         if word in context:
             # Compile a call.
-            pass
+            # The amount of space required ahead of this call.
+            space = 0x6
+            ucode += call(context[word][0], pc + len(ucode) + space)
+            # Switch back to main stack.
+            ucode += switch_to_main()
         else:
             ucode += builtin(word)
 
+    ucode += ret()
+
     context[name] = pc, ucode
     return ucode
+
 
 def tail():
     """
@@ -98,6 +189,7 @@ def tail():
     ucode = assemble(SET, I, POP)
     ucode += assemble(SET, J, POP)
     return ucode
+
 
 def builtin(word):
     """
@@ -123,9 +215,10 @@ def builtin(word):
 
     raise Exception("Don't know builtin %r" % word)
 
+
 with open("test.forth", "rb") as f:
     tokens = [t.strip() for t in f.read().split()]
-    pc = 0x2
+    pc = len(bootloader(0)) // 2
     context = {}
     while tokens:
         t, tokens = tokens[0], tokens[1:]
@@ -133,24 +226,18 @@ with open("test.forth", "rb") as f:
             name = tokens[0]
             end = tokens.index(";")
             sub = subroutine(name, tokens[1:end], pc, context)
-            # Increment PC times 2 for the word size.
-            pc += len(sub) * 2
-            if name == "main":
-                # Leave space for a trampoline which we will use to reach the
-                # end of the program.
-                pc += 0x2
+            # Add the size of the subroutine to PC.
+            pc += len(sub) // 2
+
 
 with open("test.bin", "wb") as f:
     start = context["main"][0]
-    f.write(trampoline(start))
+    boot = bootloader(start)
+    print "Bootloader: %d bytes (%d words)" % (len(boot), len(boot) // 2)
+    f.write(boot)
     for name in context:
         pc, u = context[name]
+        print "Sub %s: %d bytes (%d words) @ 0x%x" % (name, len(u),
+            len(u) // 2, pc)
+        f.seek(pc)
         f.write(u)
-        if name == "main":
-            # Set the end-of-program trampoline.
-            address = pc + len(u)
-    target = f.tell() // 2
-    f.write(tail())
-    tramp = trampoline(target)
-    f.seek(address * 2)
-    f.write(tramp)
