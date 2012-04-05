986f106307456f9355dc30ae2d8d9d0f84e7791c
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-04-05 00:29:49 -0700

    Add more tests.
    Well, I'm convinced that the assembler's not emitting bad code.

--- cauliflower/tests/test_assembler.py
+++ cauliflower/tests/test_assembler.py
@@ -1,8 +1,20 @@
 from unittest import TestCase
 
-from cauliflower.assembler import A, SET, assemble
+from cauliflower.assembler import A, PC, PUSH, SET, assemble
 
 class TestAssembler(TestCase):
+
+    def test_set_pc_literal(self):
+        expected = "\xc5\xc1"
+        self.assertEqual(expected, assemble(SET, PC, 0x11))
+
+    def test_set_pc_literal_long(self):
+        expected = "\x7d\xc1\x12\x34"
+        self.assertEqual(expected, assemble(SET, PC, 0x1234))
+
+    def test_set_push_literal(self):
+        expected = "\x89\xa1"
+        self.assertEqual(expected, assemble(SET, PUSH, 0x2))
 
     def test_set_register_literal(self):
         expected = "\x7c\x01\x00\x30"
